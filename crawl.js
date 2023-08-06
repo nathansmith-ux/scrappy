e90665cf0a2557b34968789e542029fe02fc72b8
Nathan <nathanrmsmith@gmail.com>
2023-08-05 21:54:36 -0400

    Add getURLsFromHTML function with tests

--- crawl.js
+++ crawl.js
@@ -1,6 +1,40 @@
+const { JSDOM } = require('jsdom');
+
+/**
+ * 
+ * @param {string} htmlBody // HTML of page
+ * @param {string} baseURL // URL of website we are crawling
+ * @returns An array of strings representing URLs
+ */ 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll('a')
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative URL
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`)
+        urls.push(urlObj.href)
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`)
+      }
+    } else {
+      // absolute URL
+      try {
+        const urlObj = new URL(linkElement.href)
+        urls.push(urlObj.href)
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`)
+      }
+    }
+  }
+  return urls;
+}
+
 /**
  * Function normalizes URLs using URL Object
- * @param {url} urlString 
+ * @param {url String} urlString 
  * @returns domain and path as a string
  */
 function normalizeURL(urlString) {
@@ -13,5 +47,6 @@ function normalizeURL(urlString) {
 };
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
